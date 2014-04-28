7c356e8d622769695daae0cdfb77f0a99b89c765
randlet <randle.taylor@gmail.com>
2014-04-28 10:09:23 -0400

    bump version

--- dvh/__init__.py
+++ dvh/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = 'Randle Taylor'
 __email__ = 'randle.taylor@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from dvh import DVH, monotonic_increasing, monotonic_decreasing
