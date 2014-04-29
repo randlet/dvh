7e826b5c1830e781ae1f38b98af793ecf99a23a9
randlet <randle.taylor@gmail.com>
2014-04-28 23:17:09 -0400

    finish testing dvh

--- dvh/dvh.py
+++ dvh/dvh.py
@@ -12,39 +12,45 @@ def monotonic_decreasing(list_):
     """Check if input list is monotonically decreasing"""
     return np.all(np.diff(list_) <= 0)
 
+def differential_to_cumulative(doses, volumes):
+    cum =np.cumsum(volumes[::-1])[::-1]
+    return cum
+
 
 class DVH(object):
 
-    def __init__(self, bins=None, volumes=None):
+    def __init__(self, doses=None, volumes=None):
         """
-        bins is a sequence of dose bin edges including the rightmost edge (same
-        as numpy.histogram).
-        volumes is either differential or cumulative volume corresponding to
-        each bin.
-
-        The length of bins must be exactly 1 more than volumes.
-
-        For example:
-            bins =    [0, 10, 20, 30, 40]
-            volumes = [ 0,  0.5, 0.4, 0.1]
-        would mean 50% of volume receives a dose between 10 and 20cGy
-
+        doses, volumes are arrays of equal length representing points on a
+        cumulative dose volume histogram curve.
         """
 
-        if bins is None or volumes is None:
-            raise ValueError("You must pass both bins and volumes arrays")
+        if doses is None or volumes is None:
+            raise ValueError("You must pass both doses and volumes arrays")
 
-
-        self.bins = np.array(bins, dtype=np.float)
-        if self.bins[0] != 0:
-            raise ValueError("First dose bin edge must be at 0 dose")
-
-        self.bin_mids = self.bins[:-1] + np.diff(self.bins)/2.
-
-        if not monotonic_increasing(self.bins):
+        if not monotonic_increasing(doses):
             raise ValueError("Input doses must be montonically increasing")
 
+        if not monotonic_decreasing(volumes):
+            volumes = differential_to_cumulative(doses, volumes)
+
+        if len(volumes) != len(doses):
+            raise ValueError("Mismatch between length of volumes and dose arrays")
+
+
+        self.doses = np.array(doses, dtype=np.float)
         self._volumes = np.array(volumes, dtype=np.float)
+
+        # force dose to zero for first point
+        if self.doses[0] != 0:
+            self.doses = np.insert(self.doses, 0, 0)
+            self._volumes = np.insert(self._volumes, 0, self._volumes[0])
+
+        # force volumes to zero for last point
+        if self._volumes[-1] != 0:
+            self.doses = np.append(self.doses, self.doses[-1] + (self.doses[-1] - self.doses[-2]))
+            self._volumes  = np.append(self._volumes, 0)
+
 
         self._set_volumes()
         self._calculate_stats()
@@ -52,28 +58,24 @@ class DVH(object):
 
     def _set_volumes(self):
 
-        dvh_is_cumulative = monotonic_decreasing(self._volumes) and (self._volumes[0] == self._volumes.max())
+        self.cum_volumes = self._volumes/self._volumes.max()
+        self.diff_volumes = np.append(np.diff(self.cum_volumes[::-1])[::-1], self.cum_volumes[-1])
 
-        if dvh_is_cumulative:
+        # sanity check
+        assert len(self.doses) == len(self.diff_volumes) and len(self.diff_volumes) == len(self.cum_volumes)
 
-            self.cum_volumes = self._volumes/self._volumes.max()
-            self.diff_volumes = np.append(np.diff(self.cum_volumes[::-1])[::-1], self.cum_volumes[-1])
-        else:
-            self.diff_volumes = self._volumes/self._volumes.sum()
-            self.cum_volumes = np.cumsum(self.diff_volumes[::-1])[::-1]
-
-        if len(self.bins) != len(self.diff_volumes) + 1 or len(self.diff_volumes) != len(self.cum_volumes):
-            raise ValueError(
-                "Invalid number of bins. bins should be a list of bin "
-                "edges including the right most edge. len(bins) must be "
-                "equal to len(volumes) + 1"
-            )
 
     def _calculate_stats(self):
-        self.mean_dose = (self.bin_mids * self.diff_volumes).sum()
+        self.mean_dose = (self.doses * self.diff_volumes).sum()
         nonzero = np.where(self.diff_volumes > 0)[0]
-        self.min_dose = self.bin_mids[nonzero[0]]
-        self.max_dose = self.bin_mids[nonzero[-1]]
+
+        # special case to handle Monaco min dose = 0
+        if self.doses[1] == (self.doses[2] - self.doses[1])/2:
+            self.min_dose = 0.
+        else:
+            self.min_dose = self.doses[nonzero[0]]
+
+        self.max_dose = self.doses[nonzero[-1]]
 
     def dose_to_volume_fraction(self, volume_fraction):
         """Return the dose that receives at least volume_fraction % dose (e.g.
@@ -92,14 +94,14 @@ class DVH(object):
         try:
             lower_idx = np.where(self.cum_volumes >= volume_fraction)[0][-1] #
             l, u = lower_idx, lower_idx+1
-            doses = self.bin_mids
-            volumes = self.cum_volumes
-            return doses[l] + (doses[u] - doses[l])*(volume_fraction - volumes[l]) / (volumes[u] - volumes[l])
-        except IndexError:
+            ds, vs = self.doses, self.cum_volumes
+            return ds[l] + (ds[u] - ds[l])*(volume_fraction - vs[l]) / (vs[u] - vs[l])
+        except IndexError: # pragma:nocover
+            # should never hit this since we always stick have a zero volume point on end of dvh
             raise ValueError("DVH is truncated.  Unable to calculate dose to requested volume fraction")
 
     def volume_fraction_receiving_dose(self, dose):
-        """ Return the fraction of total volume recieving the input dose. (e.g.
+        """ Return the fraction of total volume recieving at least the input dose. (e.g.
         dvh.volume_fraction_receiving_dose(50) == V50Gy ).
 
         Volumes are linearly interpreted between two enclosing points.
@@ -110,9 +112,8 @@ class DVH(object):
         elif dose <= self.min_dose:
             return 1.
 
-        lower_idx = numpy.where(self.bin_mids <= dose)[0][-1]
+        lower_idx = np.where(self.doses <= dose)[0][-1]
         l, u = lower_idx, lower_idx + 1
-        doses = self.bin_mids
-        volumes = self.cum_volumes
+        ds, vs = self.doses, self.cum_volumes
 
-        return volumes[l]+(volumes[u]-volumes[l])*(dose-doses[l])/(doses[u]-doses[l])
+        return vs[l]+(vs[u]-vs[l])*(dose-ds[l])/(ds[u]-ds[l])
