0aebefefb0f0b6b2fbd44dd7692dc2fc2207862a
Ryan Bottema <ryanbottema@gmail.com>
2023-04-06 12:01:02 -0400

    version

--- dvh/__init__.py
+++ dvh/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = 'Randle Taylor'
 __email__ = 'randle.taylor@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 from .dvh import DVH, monotonic_increasing, monotonic_decreasing
